62b4551c9c1e908d8a832ba9448109977d0fc414
Mesut Cevik <mesut.cevik@telekom.de>
2019-08-20 14:10:05 +0200

    NotNone

--- Miscellaneous/NotNone.py
+++ Miscellaneous/NotNone.py
@@ -7,32 +7,39 @@ None is not the same as 0, False, or an empty string. None is a datatype of its 
 """
 
 
-def is_none_or_not_none(x: Any):
-    print()
+def is_none_or_not_none_a(x: Any):
+    # This function proves whether x is not None (it has any value or zero) or
+    # it is None (it has no value not even a zero).
     if x is not None:
-        print(f"The value of x is: {x}. It has a value, it is not 'None'.")
+        print(f"x = {x}. x is not None.")
+        # If x has not stored 'None' then do this.
     else:
-        print(f"The value of x is: {x}. It has no value, it is 'None'.")
+        print(f"x = {x}. x is None, it has stored None. You see this text only when x has stored None.")
+        # If x has stored 'None' then do this.
+
+
+def is_none_or_not_none_b(x: Any):
+    # This function proves whether x is True or False.
+    if x:
+        print(f"x = {x}. x is True, it has stored not 0, None or something empty.")
+        # If x is True then do this. x is True when it has a value which is not 0 (zero) and not empty.
+    else:
+        print(f"x = {x}. x is False, it has stored 0, None or something empty.")
+        # If x is False, that means x is not Ture, then do this. x is False when it has 0 (zero), None or an empty string.
 
 
 if __name__ == '__main__':
-    # The VAR y has the value 7 - that means it is not None.
-    is_none_or_not_none(7)
+    is_none_or_not_none_a(7)  # The VAR y is not None, it has the value 7.
+    is_none_or_not_none_a(0)  # The VAR y None ( value 0 - that means it is not None.
+    is_none_or_not_none_a(-5)  # The VAR y None ( value 0 - that means it is not None.
+    is_none_or_not_none_a("")  # The VAR y has the value 0 - that means it is not None.
+    is_none_or_not_none_a("g")  # The VAR y has the value 0 - that means it is not None.
+    is_none_or_not_none_a(None)  # The VAR x has no value - that means it is None.
 
-    # The VAR y has the value 0 - that means it is not None.
-    is_none_or_not_none(0)
-
-    # The VAR x has no value - that means it is None.
-    is_none_or_not_none(None)
-
-    # The VAR x has no string value - that means it is None.
-    is_none_or_not_none('')
-
-    # The VAR x has no string value - that means it is None.
-    is_none_or_not_none([])
-
-    # The VAR x has no string value - that means it is None.
-    is_none_or_not_none({})
-
-    # The VAR x has no string value - that means it is None.
-    is_none_or_not_none(())
+    print()
+    is_none_or_not_none_b(7)  # The VAR y has the value 7 - that means it is not None.
+    is_none_or_not_none_b(0)  # The VAR y has the value 0 - that means it is not None.
+    is_none_or_not_none_b(-5)  # The VAR y has the value 0 - that means it is not None.
+    is_none_or_not_none_b("")  # The VAR x has no value - that means it is None.
+    is_none_or_not_none_b("g")  # The VAR x has no value - that means it is None.
+    is_none_or_not_none_b(None)  # The VAR x has no value - that means it is None.
